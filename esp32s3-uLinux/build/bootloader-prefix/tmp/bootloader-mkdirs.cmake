ffe2581fe7ccbb237d9773d1e5d430d0ff7cd558
RaymondBello <raymondbello11@gmail.com>
2023-12-30 19:52:33 -0500

    first commit

---
+++ esp32s3-uLinux/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/Users/raybello/esp/esp-idf/components/bootloader/subproject"
+  "/Users/raybello/esp/esp32s3-linux/esp32s3-uLinux/build/bootloader"
+  "/Users/raybello/esp/esp32s3-linux/esp32s3-uLinux/build/bootloader-prefix"
+  "/Users/raybello/esp/esp32s3-linux/esp32s3-uLinux/build/bootloader-prefix/tmp"
+  "/Users/raybello/esp/esp32s3-linux/esp32s3-uLinux/build/bootloader-prefix/src/bootloader-stamp"
+  "/Users/raybello/esp/esp32s3-linux/esp32s3-uLinux/build/bootloader-prefix/src"
+  "/Users/raybello/esp/esp32s3-linux/esp32s3-uLinux/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/Users/raybello/esp/esp32s3-linux/esp32s3-uLinux/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/Users/raybello/esp/esp32s3-linux/esp32s3-uLinux/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
